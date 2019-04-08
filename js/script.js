405260a21644802853f77c1cab85d028bcf426ff
Lhourquin <lhourquin@gmail.com>
2019-04-08 17:24:50 +0200

    insert variable and function addEventListener

--- js/script.js
+++ js/script.js
@@ -1,4 +1,17 @@
 'use strict';
 
-let ul = getElementsByTagName("ul");
+let ul = document.getElementsByTagName("ul")[0];
+let previous = document.getElementsByClassName('previous');
+let next = document.getElementsByClassName('next');
 
+
+ul.addEventListener("click", function (event){
+    console.log("str");
+    let target = event.target;
+    console.log(target);
+    if(target != 'next'){
+        return;
+    }else{
+        ul.children.toggle('display');
+    }
+});
