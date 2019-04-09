047ad185999d8d2e5a3ec9ac01e88a7a741d8268
Lhourquin <lhourquin@gmail.com>
2019-04-09 10:56:28 +0200

    test function event onclick for display img with event target

--- js/script.js
+++ js/script.js
@@ -3,15 +3,12 @@
 let ul = document.getElementsByTagName("ul")[0];
 let previous = document.getElementsByClassName('previous');
 let next = document.getElementsByClassName('next');
+let img = document.getElementsByTagName('img');
 
-
-ul.addEventListener("click", function (event){
-    console.log("str");
+ul.onclick = function (event){
     let target = event.target;
     console.log(target);
-    if(target != 'next'){
-        return;
-    }else{
-        ul.children.toggle('display');
-    }
-});
+    target.classList.add('display');
+
+    
+};       
