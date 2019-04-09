207cb795c9adbf6f1e66245c932ae635b3a81ce7
Lhourquin <lhourquin@gmail.com>
2019-04-10 01:30:52 +0200

    resolve bug, function nextImg ok

--- js/script.js
+++ js/script.js
@@ -7,20 +7,15 @@ let length = listImg.length -1;
 
 function imgNext() {
     console.log(count);
-   /* listImg[count].classList.add('displayNone');
-    listImg[count +1].classList.remove('displayNone');
-    
-    ++count;*/
-
     if(count >= length ){
-  /*  listImg[length].classList.remove('displayNone');
-    count = 0;
-    listImg[count].classList.add('displayNone');*/
-    }else{
-        listImg[count].classList.add('displayNone');
-    listImg[count +1].classList.remove('displayNone');
+      listImg[length].classList.add('displayNone');
+      count = 0;
+      listImg[count].classList.remove('displayNone');
+    }else {
+      listImg[count].classList.add('displayNone');
+      listImg[count +1].classList.remove('displayNone');
     
-    ++count;
+      ++count;
     }
     console.log(count);
 }
