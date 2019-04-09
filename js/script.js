96972a798a0d8756c888bb2e6a3a70920f54c452
Lhourquin <lhourquin@gmail.com>
2019-04-10 01:11:05 +0200

    function nextImg 3

--- js/script.js
+++ js/script.js
@@ -3,23 +3,24 @@
 
 let listImg = document.getElementsByClassName('imgCarousel');
 let count = 0;
-let length = listImg.length - 1;
+let length = listImg.length -1;
 
 function imgNext() {
+    console.log(count);
+   /* listImg[count].classList.add('displayNone');
+    listImg[count +1].classList.remove('displayNone');
     
-    listImg[count].classList.add('displayNone');
+    ++count;*/
+
+    if(count >= length ){
+  /*  listImg[length].classList.remove('displayNone');
+    count = 0;
+    listImg[count].classList.add('displayNone');*/
+    }else{
+        listImg[count].classList.add('displayNone');
     listImg[count +1].classList.remove('displayNone');
     
     ++count;
-
-    if(count >= length ){
-    listImg[length].classList.add('displayNone');
-    count = 0;
-    listImg[count].classList.remove('displayNone');
-    //listImg[count.length].classList.add('displayNone');
-    
-    
-    
     }
     console.log(count);
 }
