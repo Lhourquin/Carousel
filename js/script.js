aebb2f16e6142af352a12e92b7498898d39a8f2e
Lhourquin <lhourquin@gmail.com>
2019-04-09 22:21:31 +0200

    function imgNext bug

--- js/script.js
+++ js/script.js
@@ -3,12 +3,20 @@
 
 let listImg = document.getElementsByClassName('imgCarousel');
 let count = 0;
+let length = listImg.length;
 
 function imgNext() {
     
     listImg[count].classList.add('displayNone');
     listImg[count +1].classList.remove('displayNone');
+    
+    ++ count;
 
-    ++ count;    
-        
+    if(count >= length-1){
+    
+    listImg[count].classList.add('displayNone');
+    count = 0;
+    listImg[count=0].classList.remove('displayNone');
     }
+    console.log(count);
+}
